e2f79dcbc0a3387038e91722071cf691d1ce5eab
ChavezCode <jch1248@wgu.edu>
2025-02-16 20:28:31 -0800

    first commit

---
+++ src/main/java/com/store/tree/shop/TreeShopApplication.java
@@ -0,0 +1,13 @@
+package com.store.tree.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/tree/shop/TreeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.tree.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
